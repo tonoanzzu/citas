e352856fd993eaa57a12293cf7b6bd018a5e7f2f
tonoanzzu <tono.anzzu@gmail.com>
2020-03-26 01:41:19 -0600

    Uso de esqueleton para crear formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
+import Formulario from './Formulario';
 
 
-class App extends Component {
-  render() {
+function  App()  {
+  
     return (
+      <Fragment>
       <h1>Administrador de pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+          <Formulario />
+          </div>
+          <div className="one-half column">
+            II
+          </div>
+        </div>
+
+      </div>
+      </Fragment>
     );
-  }
+  
 }
 
 export default App;
---
+++ src/Formulario.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Formulario = () => {
+    return (
+        <h1>Desde formulario</h1>
+      );
+}
+ 
+export default Formulario;
